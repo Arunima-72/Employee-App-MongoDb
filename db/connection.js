50af41cffa8f0e6b96527e74d32c501967ffa7b5
Arunima-72 <arunimasuresh89@gmail.com>
2025-04-27 15:29:44 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 require('dotenv').config()
 // connecting db
-mongoose.connect('mongodb+srv://Arunima:Arunima72@cluster0.dwfhkqr.mongodb.net/EmployeeDb?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
+mongoose.connect('mongodb+srv://newusername:newpassword@cluster0.dwfhkqr.mongodb.net/EmployeeDb?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
     console.log('Connection established')
 }).catch(()=>{
 console.log('connection error')
